97e7a44dff6205def4073c9cce00cb56651caffd
Ehsan Hakam <ehsan.hakam@globalnorthstar.com>
2025-12-16 16:25:08 +0500

    Event reservation creation from android

---
+++ Northstar/Scripts/EventReservationFromCatalog/Script1765880116273.groovy
@@ -0,0 +1,45 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('D:\\APK\\CN30EURL.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - Continue to Login'), 0)
+
+Mobile.sendKeys(findTestObject('Object Repository/android.widget.EditText'), '10007')
+
+Mobile.sendKeys(findTestObject('Object Repository/android.widget.EditText (1)'), '123456')
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - Sign In'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.ImageView'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - Events Catalogue'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.TextView - Yoga with Yogesh - Series'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - Choose Days  Times'), 0)
+
+Mobile.swipe(540, 1800, 540, 600)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - Create Reservation'), 0)
+
+Mobile.takeScreenshot('C:\\Users\\EHSANH~1\\AppData\\Local\\Temp\\screenshot16689980727093340990.png')
+
+Mobile.closeApplication()
+
