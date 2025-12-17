acc6774f4aeadbe6509da400d660aa33dca938c0
subhan.hassan@globalnorthstar.com <katalon_g3_dummy_username>
2025-12-17 10:55:19 +0000

    Create "WebRunWithNasar" test suite

---
+++ Test Suites/WebRunWithNasar.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+    <name>WebRunWithNasar</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>3</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>true</rerunImmediately>
+    <testSuiteGuid>5be1da29-da5d-4894-ad33-f0a1249ed0cc</testSuiteGuid>
+    <testCaseLink>
+        <guid>a83b02bc-928b-43bd-b21e-853c966a6c7f</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Login/login</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+    </testCaseLink>
+</TestSuiteEntity>
