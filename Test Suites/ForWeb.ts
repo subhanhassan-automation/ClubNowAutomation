ed04586831a7958e5efd897e1a985f9aec34bd8e
subhan.hassan@globalnorthstar.com <katalon_g3_dummy_username>
2025-12-17 09:23:41 +0000

    Edit "ForWeb" test suite

--- Test Suites/ForWeb.ts
+++ Test Suites/ForWeb.ts
@@ -25,4 +25,37 @@
       <testCaseId>Test Cases/Events/Create Event</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
+   <testCaseLink>
+      <guid>81a6891b-330a-4244-9a8a-d902d1af5f3a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AP/Create Vendor</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>db507ec2-8fa1-4b41-aebd-51305e73b2b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AP/Create Payment type with Check Type</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLinks>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>fc2ee0b3-08c3-4349-8b9c-ac5df6a9cfd1</variableId>
+      </variableLinks>
+      <variableLinks>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0a359044-3d52-4a77-b67a-027f21da17da</variableId>
+      </variableLinks>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>162ed406-e670-4da6-ae60-288ab1b8e124</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AP/Create Invoice til Finalize</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
