c7f7c276cfe832b4465db3d645512d70690f9d40
subhanhassan-automation <subhan.hassan@globalnorthstar.com>
2025-12-22 22:16:06 +0500

    add sample testsuite

---
+++ Test Suites/SampleTS.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SampleTS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6de524e1-4930-4da0-a3a8-a3fd18096d38</testSuiteGuid>
+</TestSuiteEntity>
