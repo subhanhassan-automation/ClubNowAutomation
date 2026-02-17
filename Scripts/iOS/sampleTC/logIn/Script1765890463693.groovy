cdff9f1d480cb984f4fd32f29ab595aff1a519c7
sheikhadnan-art <sheikh.adnan@globalnorthstar.com>
2026-02-17 17:37:31 +0500

    Revert "dynamic login"

--- Scripts/iOS/sampleTC/logIn/Script1765890463693.groovy
+++ Scripts/iOS/sampleTC/logIn/Script1765890463693.groovy
@@ -17,51 +17,27 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-Mobile.startApplication('/Users/muhammadali/Downloads/nsplus.app', false)
+Mobile.startApplication('/Users/muhammadali/Downloads/nsplus.app', true)
 
 Mobile.tap(findTestObject('Object Repository/XCUIElementTypeTextField - LandingViewController.txtFieldClubName'), 0)
+
 Mobile.sendKeys(findTestObject('Object Repository/XCUIElementTypeTextField - LandingViewController.txtFieldClubName'), 'aqdas')
 
 Mobile.tap(findTestObject('Object Repository/XCUIElementTypeButton - LandingViewController.connectBtn'), 0)
+
 Mobile.tap(findTestObject('Object Repository/XCUIElementTypeButton - Continue to Login'), 0)
 
-// 🔹 Get placeholder or value of first login field
-String fieldType = Mobile.getAttribute(
-    findTestObject('Object Repository/XCUIElementTypeTextField - Member Number'),
-    'value',
-    5
-)
+Mobile.tap(findTestObject('Object Repository/XCUIElementTypeTextField - Member Number'), 0)
 
-println('Field Type Found: ' + fieldType)
+Mobile.sendKeys(findTestObject('Object Repository/XCUIElementTypeTextField - Member Number'), '10124')
 
-// 🔹 Condition logic
-if (fieldType.equalsIgnoreCase('Member Number')) {
+Mobile.tap(findTestObject('Object Repository/XCUIElementTypeSecureTextField - Password'), 0)
 
-    Mobile.sendKeys(findTestObject('Object Repository/XCUIElementTypeTextField - Member Number'), '10124')
+Mobile.sendKeys(findTestObject('Object Repository/XCUIElementTypeSecureTextField - Password'), '123456')
 
-    Mobile.sendKeys(findTestObject('Object Repository/XCUIElementTypeSecureTextField - Password'), '123456')
-
-    Mobile.tap(findTestObject('Object Repository/XCUIElementTypeButton - Sign In'), 0)
-
-    println('Logged in using Member Number')
-
-} else if (fieldType.equalsIgnoreCase('Email Address')) {
-
-    Mobile.sendKeys(findTestObject('Object Repository/XCUIElementTypeTextField - Member Number'), 'test@globalnorthstar.com')
-
-    Mobile.sendKeys(findTestObject('Object Repository/XCUIElementTypeSecureTextField - Password'), '123456')
-
-    Mobile.tap(findTestObject('Object Repository/XCUIElementTypeButton - Sign In'), 0)
-
-    println('Logged in using Email')
-
-} else {
-
-    println('Unknown login type detected')
-}
+Mobile.tap(findTestObject('Object Repository/XCUIElementTypeButton - Sign In'), 0)
 
 Mobile.tap(findTestObject('Object Repository/XCUIElementTypeButton - Allow'), 0)
 
 Mobile.closeApplication()
-
 
