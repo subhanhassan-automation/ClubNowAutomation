8370c2267a7a407524c4226f29dcc652bcd0512c
sheikhadnan-art <sheikh.adnan@globalnorthstar.com>
2026-02-19 16:01:38 +0500

    Fixes: ImplementationOfDynamicDesiredCapabilities

--- Scripts/Android/Login/Login Member/Script1770724514733.groovy
+++ Scripts/Android/Login/Login Member/Script1770724514733.groovy
@@ -1,8 +1,39 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.util.KeywordUtil
+import internal.GlobalVariable
 
-Mobile.startApplication('D:\\APK\\ClubNow-Android.apk', true)
+KeywordUtil.logInfo("Applying mobile caps from Global Variables: " +
+	"ignoreHiddenApiPolicyError=${GlobalVariable.ignoreHiddenApiPolicyError}, " +
+	"PlatformName=${GlobalVariable.platformName}, " +
+	"AutomationName=${GlobalVariable.automationName}, " +
+	"AutoGrantPermissions=${GlobalVariable.autoGrantPermissions}, " +
+	"DontStopAppOnReset=${GlobalVariable.dontStopAppOnReset}, " +
+	"AdbExecTimeout=${GlobalVariable.adbExecTimeout}, " +
+	"AndroidInstallTimeout=${GlobalVariable.androidInstallTimeout}, " +
+	"Uiautomator2ServerLaunchTimeout=${GlobalVariable.uiautomator2ServerLaunchTimeout}, " +
+	"NewCommandTimeout=${GlobalVariable.newCommandTimeout}, " +
+	"AppWaitDuration=${GlobalVariable.appWaitDuration}, " +
+	"app=${GlobalVariable.appPath}, " +
+	"Uiautomator2ServerInstallTimeout=${GlobalVariable.uiautomator2ServerInstallTimeout}")
+
+RunConfiguration.setMobileDriverPreferencesProperty('PlatformName', GlobalVariable.platformName)
+RunConfiguration.setMobileDriverPreferencesProperty('AutomationName', GlobalVariable.automationName)
+RunConfiguration.setMobileDriverPreferencesProperty('AutoGrantPermissions', GlobalVariable.autoGrantPermissions)
+RunConfiguration.setMobileDriverPreferencesProperty('ignoreHiddenApiPolicyError', GlobalVariable.ignoreHiddenApiPolicyError)
+RunConfiguration.setMobileDriverPreferencesProperty('DontStopAppOnReset', GlobalVariable.dontStopAppOnReset)
+RunConfiguration.setMobileDriverPreferencesProperty('AdbExecTimeout', GlobalVariable.adbExecTimeout)
+RunConfiguration.setMobileDriverPreferencesProperty('AndroidInstallTimeout', GlobalVariable.androidInstallTimeout)
+RunConfiguration.setMobileDriverPreferencesProperty('Uiautomator2ServerInstallTimeout', GlobalVariable.uiautomator2ServerInstallTimeout)
+RunConfiguration.setMobileDriverPreferencesProperty('Uiautomator2ServerLaunchTimeout', GlobalVariable.uiautomator2ServerLaunchTimeout)
+RunConfiguration.setMobileDriverPreferencesProperty('AppWaitDuration', GlobalVariable.appWaitDuration)
+RunConfiguration.setMobileDriverPreferencesProperty('NewCommandTimeout', GlobalVariable.newCommandTimeout)
+RunConfiguration.setMobileDriverPreferencesProperty('app', GlobalVariable.appPath)
+
+
+Mobile.startApplication(GlobalVariable.appPath, true)
 
 Mobile.tap(findTestObject('Object Repository/Test Cases/android.webkit.WebView (6)'), 0)
 
