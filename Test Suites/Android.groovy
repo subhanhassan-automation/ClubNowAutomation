09cec10242220ec8257979c9ef0800c6d3193176
Muhammad Shahzaib Laique <shahzaib.laique@globalnorthstar.com>
2026-02-20 14:36:32 +0500

    Navigation to event page

--- Test Suites/Android.groovy
+++ Test Suites/Android.groovy
@@ -23,6 +23,51 @@ import com.kms.katalon.core.annotation.SetUp
 import com.kms.katalon.core.annotation.SetupTestCase
 import com.kms.katalon.core.annotation.TearDown
 import com.kms.katalon.core.annotation.TearDownTestCase
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
+import io.appium.java_client.AppiumDriver
+import org.openqa.selenium.Dimension
+import com.kms.katalon.core.model.FailureHandling
+
+// ===== START APPLICATION =====
+Mobile.startApplication('D:\\ClubNow-Android.apk', true)
+
+// ===== TAP MENU =====
+Mobile.tap(findTestObject('Object Repository/Test Cases/android.widget.ImageView (5)'), 10)
+
+// ===== SCROLL UNTIL FOUND =====
+boolean found = false
+int maxScroll = 5
+
+AppiumDriver driver = MobileDriverFactory.getDriver()
+Dimension size = driver.manage().window().getSize()
+
+int startX = size.width / 2
+int startY = (int)(size.height * 0.8)
+int endY   = (int)(size.height * 0.2)
+
+for (int i = 0; i < maxScroll; i++) {
+
+	if (Mobile.verifyElementExist(findTestObject('Object Repository/Test Cases/Profile Element'), 2, FailureHandling.OPTIONAL)) {
+		found = true
+		break
+	}
+
+	Mobile.swipe(startX, startY, startX, endY)
+	Mobile.delay(2)
+}
+
+// ===== TAP IF FOUND =====
+if (found) {
+	Mobile.tap(findTestObject('Object Repository/Test Cases/Profile Element'), 10)
+	println("Profile found and tapped")
+} else {
+	println("Element not found after scrolling")
+}
+
+// ===== CLOSE APP =====
+Mobile.closeApplication()
 
 /**
  * Some methods below are samples for using SetUp/TearDown in a test suite.
@@ -60,6 +105,41 @@ def tearDownTestCase() {
 	// Put your code here.
 }
 
+// ===== START APPLICATION =====
+Mobile.startApplication('D:\\ClubNow-Android.apk', true)
+
+// ===== TAP MENU =====
+Mobile.tap(findTestObject('Object Repository/Test Cases/android.widget.ImageView (5)'), 10)
+
+// ===== SCROLL UNTIL FOUND =====
+boolean found = false
+int maxScroll = 5
+
+AppiumDriver driver = MobileDriverFactory.getDriver()
+Dimension size = driver.manage().window().getSize()
+
+int startX = size.width / 2
+int startY = (int)(size.height * 0.8)
+int endY   = (int)(size.height * 0.2)
+
+for (int i = 0; i < maxScroll; i++) {
+
+	if (Mobile.verifyElementExist(findTestObject('Object Repository/Test Cases/Profile Element'), 2, FailureHandling.OPTIONAL)) {
+		found = true
+		break
+	}
+
+	Mobile.swipe(startX, startY, startX, endY)
+	Mobile.delay(2)
+}
+
+// ===== TAP IF FOUND =====
+if (found) {
+	Mobile.tap(findTestObject('Object Repository/Test Cases/Profile Element'), 10)
+	println("Profile found and tapped")
+} else {
+	println("Element not found after scrolling")
+}
 /**
  * References:
  * Groovy tutorial page: http://docs.groovy-lang.org/next/html/documentation/
